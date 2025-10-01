bc5cd45c261964d874cc4247c4b52f8720305699
Oyekola Toheeb <toheeb.olawale.to23@gmail.com>
2025-10-01 07:37:04 +0100

    feat: add queue worker

--- routes/console.php
+++ routes/console.php
@@ -4,9 +4,15 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote');
 
 Schedule::command('disposable:update')->weekly();
 Schedule::command(' app:generate-sitemap')->daily();
+
+
+Schedule::command('queue:work --tries=3 --max-time=3600 --stop-when-empty')
+    ->everyMinute()
+    ->withoutOverlapping()
+    ->sendOutputTo(storage_path('/logs/queue-jobs.log'));
